6e905464eee53f55a8a8527dac304f38380561b7
David <cadillodavid@gmail.com>
2017-06-13 12:43:49 -0500

    Formulario listo al 85%. Se ha borrado los drawables de verificación y se ha pasado a usar svg.

---
+++ app/src/main/java/com/tusueldo/comodin/utils/ComodinPattern.java
@@ -0,0 +1,33 @@
+package com.tusueldo.comodin.utils;
+
+import android.util.Patterns;
+
+/**
+ * Created by USUARIO on 13/06/2017.
+ */
+
+public class ComodinPattern {
+
+
+    /*Expresión regular para validar el formato de la fecha de nacimiento con el formato dd/mm/yyyy teniendo en cuenta
+    * los dígitos adecuados para mes, año y día.
+    * */
+    public static final String FECHA_NACIMIENTO = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\\d\\d$";
+
+    /*Expresión regular usado para validar un celular con el formato 9XXXXXXXXX*/
+    public static final String CELULAR = "9\\d{8}";
+
+    /*Expresión regular para validar un email. Se usa el que viene con Android*/
+    public static final String EMAIL = Patterns.EMAIL_ADDRESS.pattern();
+
+    /*Expresión regular para el nombre en el que se aceptan todas las letras del abecedarios, las tildes de las letras,
+    los espacios y el guion.
+    El nombre como mínimo ha de ser de 3 letras y máximo de 30. Se permiten nombres compuestos
+    * */
+
+    public static final String NOMBRES = "([a-zA-Záéíóú]+['\\s-]?){1,5}";
+
+    /*Expresión regular usada para detectar al menos un dígito en una cadena*/
+    public static final String TIENE_ALGUN_NUMERO = ".*\\d+.*";
+
+}
--- app/src/main/java/com/tusueldo/comodin/utils/ComodinValidator.java
+++ app/src/main/java/com/tusueldo/comodin/utils/ComodinValidator.java
@@ -1,22 +1,15 @@
 package com.tusueldo.comodin.utils;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.ContextCompat;
-import android.text.Editable;
 import android.text.TextUtils;
 import android.util.Log;
-import android.util.Patterns;
-import android.view.View;
 import android.widget.ImageView;
 import com.tusueldo.comodin.R;
-import com.tusueldo.comodin.SignupActivity;
 
 import java.util.regex.Pattern;
-
-import static com.tusueldo.comodin.R.id.img_nombres;
-import static com.tusueldo.comodin.R.id.img_telefono;
-import static com.tusueldo.comodin.R.id.til_telefono;
 
 /**
  * Created by USUARIO on 09/06/2017.
@@ -28,91 +21,93 @@ public class ComodinValidator {
     private static boolean apellidoValidado = false;
     private static boolean telefonoValidado = false;
     private static boolean fechaValidada = false;
-    private static boolean correoValidado;
+    private static boolean correoValidado = false;
 
 
     public static void validateNombre(Context context, CharSequence campo_nombre, TextInputLayout til_nombre, ImageView img_nombres) {
-        String nombre = campo_nombre.toString().trim();
-
-        if (nombre.isEmpty()) {
-            markEmpty(context, til_nombre, img_nombres, "Nombre no puede quedar vacío.");
-            img_nombres.setColorFilter(ContextCompat.getColor(context, R.color.colorPrimaryDark));
+        String nombre = getTrim(campo_nombre).toLowerCase();
+        boolean validarNombre = Pattern.matches(ComodinPattern.NOMBRES, nombre);
+        if (Pattern.matches(ComodinPattern.TIENE_ALGUN_NUMERO, nombre)) {
+            markEmpty(context, til_nombre, img_nombres, "No números");
+        } else if (nombre.length() == 0) {
+            markEmpty(context, til_nombre, img_nombres, "No puede quedar vacío.");
+        } else if (!validarNombre) {
+            markEmpty(context, til_nombre, img_nombres, "No permitido.");
         } else if (nombre.length() < 3) {
-//            markEmpty(context, til_nombre, img_nombres, "El nombre tiene al menos 3 letras.");
             markEmpty(context, til_nombre, img_nombres, "Mínimo 3 letras.");
-            img_nombres.setColorFilter(ContextCompat.getColor(context, R.color.colorPrimaryDark));
         } else if (nombre.length() > til_nombre.getCounterMaxLength()) {
-            markEmpty(context, til_nombre, img_nombres, "Número de caracteres no permitidos");
-            img_nombres.setColorFilter(ContextCompat.getColor(context, R.color.colorPrimaryDark));
-        } else {
-            markValidate(til_nombre);
-            //img_nombres.setColorFilter(ContextCompat.getColor(context, R.color.colorValidado));
+            markEmpty(context, til_nombre, img_nombres, "Exceso de caracteres.");
+        } else if (validarNombre) {
             nombreValidado = true;
+            markValidate(til_nombre);
             if (apellidoValidado) {
                 addDrawableValidado(context, img_nombres);
             }
         }
-
     }
 
     public static void validateApellido(Context context, CharSequence campo_apellido, TextInputLayout til_apellido, ImageView img_nombres) {
-        String apellido = campo_apellido.toString().trim();
-        if (apellido.isEmpty() || apellido == null) {
-            markEmpty(context, til_apellido, img_nombres, "Apellidos no pueden quedar vacíos.");
-            img_nombres.setColorFilter(ContextCompat.getColor(context, R.color.colorPrimaryDark));
+        String apellido = getTrim(campo_apellido);
+        boolean validarApellido = Pattern.matches(ComodinPattern.NOMBRES, apellido);
+        if (Pattern.matches(ComodinPattern.TIENE_ALGUN_NUMERO, apellido)) {
+            markEmpty(context, til_apellido, img_nombres, "No números");
+        } else if (apellido.length() == 0) {
+            markEmpty(context, til_apellido, img_nombres, "No puede quedar vacío.");
+        } else if (!validarApellido) {
+            markEmpty(context, til_apellido, img_nombres, "No permitido.");
         } else if (apellido.length() < 3) {
-            markEmpty(context, til_apellido, img_nombres, "El apellido tiene al menos 3 letras.");
-            img_nombres.setColorFilter(ContextCompat.getColor(context, R.color.colorPrimaryDark));
+            markEmpty(context, til_apellido, img_nombres, "Mínimo 3 letras.");
         } else if (apellido.length() > til_apellido.getCounterMaxLength()) {
-            markEmpty(context, til_apellido, img_nombres, "Número de caracteres no permitidos");
-            img_nombres.setColorFilter(ContextCompat.getColor(context, R.color.colorPrimaryDark));
-        } else {
-            markValidate(til_apellido);
+            markEmpty(context, til_apellido, img_nombres, "Exceso de caracteres.");
+        } else if (validarApellido) {
             apellidoValidado = true;
+            markValidate(til_apellido);
             if (nombreValidado) {
                 addDrawableValidado(context, img_nombres);
             }
         }
+
+
     }
 
     public static void validateCorreo(Context context, CharSequence campo_email, TextInputLayout til_correo, ImageView img_correo) {
-        String correo = campo_email.toString().trim();
-        correoValidado = !TextUtils.isEmpty(campo_email) && Patterns.EMAIL_ADDRESS.matcher(correo).matches();
+        String correo = getTrim(campo_email).toLowerCase();
+        correoValidado = !TextUtils.isEmpty(campo_email) && Pattern.matches(ComodinPattern.EMAIL, correo);
         if (correoValidado) {
             markValidate(til_correo);
             addDrawableValidado(context, img_correo);
         } else {
             markEmpty(context, til_correo, img_correo, "No es un correo válido.");
+            Log.d("TAG", ComodinPattern.EMAIL);
         }
-
     }
 
-
     public static void validateTelefono(Context context, CharSequence campo_telefono, TextInputLayout til_telefono, ImageView img_telefono) {
-        //String regexp = "\\d{1,2}/\\d{1,2}/\\d{4}";
-        String patternRegexPhone = "9\\d{8}";
-        String telefono = campo_telefono.toString().trim();
+        String telefono = getTrim(campo_telefono);
         boolean esVacio = !TextUtils.isEmpty(telefono);
-
-        telefonoValidado = esVacio && Pattern.matches(patternRegexPhone, telefono);
+        telefonoValidado = esVacio && Pattern.matches(ComodinPattern.CELULAR, telefono);
         if (telefonoValidado) {
             markValidate(til_telefono);
             addDrawableValidado(context, img_telefono);
         } else {
-            markEmpty(context, til_telefono, img_telefono, "No es correcto");
+            markEmpty(context, til_telefono, img_telefono, "Incorrecto");
         }
     }
 
     public static void validateFechaNac(Context context, CharSequence campo_fecha_nac, TextInputLayout til_fecha_nac, ImageView img_fecha_nac) {
-        String patternRegexFecha = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\\d\\d$";
-        String fecha_nac = campo_fecha_nac.toString().trim();
-        fechaValidada = !TextUtils.isEmpty(fecha_nac) && Pattern.matches(patternRegexFecha, fecha_nac) && ComodinDateValidator.isThisDateValid(fecha_nac);
+        String fecha_nac = getTrim(campo_fecha_nac);
+        fechaValidada = !TextUtils.isEmpty(fecha_nac) && Pattern.matches(ComodinPattern.FECHA_NACIMIENTO, fecha_nac) && ComodinDateValidator.isThisDateValid(fecha_nac);
         if (fechaValidada) {
             markValidate(til_fecha_nac);
             addDrawableValidado(context, img_fecha_nac);
         } else {
             markEmpty(context, til_fecha_nac, img_fecha_nac, "No es correcto");
         }
+    }
+
+    @NonNull
+    private static String getTrim(CharSequence campo_fecha_nac) {
+        return campo_fecha_nac.toString().trim();
     }
 
 
@@ -129,7 +124,7 @@ public class ComodinValidator {
         campo.setErrorTextAppearance(R.style.Validado);
         campo.setErrorEnabled(true);
         campo.setError(" ");
-        campo.getEditText().setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_check_circle_black_24dp, 0);
+        campo.getEditText().setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_check_circle_black_24px, 0);
     }
 
     private static void addDrawableValidado(Context context, ImageView image) {
